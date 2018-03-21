e733e863c3c5334d5d00d1c8f9dd99bfc61d8b04
samaranin <samaranin2912@gmail.com>
2018-03-21 10:57:36 +0200

    added base data to files

---
+++ gui.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PyQt5.QtWidgets import *
+
+class QHLine(QFrame):
+    def __init__(self):
+        super(QHLine, self).__init__()
+        self.setFrameShape(QFrame.HLine)
+        self.setFrameShadow(QFrame.Sunken)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()  
+        
+
+    def initUI(self):
+        #set status bar
+        self.statusBar().showMessage('Ready')
+
+        #create functional button and add event to it
+        calculate_btn = QPushButton('Calculate', self)
+        calculate_btn.clicked.connect(self.calculate_event)
+
+        #add table widget
+        self.tableResult = QTableWidget()
+
+        #create layout for result table
+        vboxTabelResult = QVBoxLayout()
+        vboxTabelResult.addWidget(QLabel('Result: '))
+        vboxTabelResult.addWidget(self.tableResult)
+
+        #create horizontal layout for button and move it to right side
+        contolButtons = QHBoxLayout()
+        contolButtons.addStretch(1)
+        contolButtons.addWidget(calculate_btn)
+
+        #join both layouts in one vertical
+        vbox = QVBoxLayout()
+
+        vbox.addLayout(vboxTabelResult)
+        vbox.addLayout(contolButtons)
+
+        #create new widget and add layout to it
+        central_widget = QWidget()
+        central_widget.setLayout(vbox) 
+
+        #draw and show main window
+        self.setCentralWidget(central_widget)
+        self.setGeometry(150, 150, 550, 150)
+        self.setWindowTitle('Soil Moisture')
+        self.show()
+
+    def calculate_event(self, button):
+        pass
---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt5.QtWidgets import *
+import gui
+import Infiltration as inf
+
+
+if __name__ == "__main__":
+    print(inf.c_i)
+
+    #show main window
+    #app = QApplication(sys.argv)
+    #ex = gui.MainWindow()
+    #sys.exit(app.exec_())
